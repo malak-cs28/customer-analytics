9dfef9018424b2b9f20835e07b32cb7eeddacec8
219-a <M.Wael2342@nu.edu.eg>
2026-03-23 21:43:02 +0200

    modified summary.sh

---
+++ summary.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONTAINER_NAME=customer_analytics_container
+
+mkdir -p results
+
+docker cp $CONTAINER_NAME:/app/pipeline/data_raw.csv ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/data_preprocessed.csv ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/insight1.txt ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/insight2.txt ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/insight3.txt ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/summary_plot.png ./results/
+docker cp $CONTAINER_NAME:/app/pipeline/clusters.txt ./results/
+
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+
+echo "All output files copied to results/ and container removed."
